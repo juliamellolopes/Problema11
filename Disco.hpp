4f8ea353809316cc9c63539b8cbbf54dbc458f9e
juliamellolopes <juliamellolopesgoncalves@gmail.com>
2021-07-11 10:35:35 -0300

    Nova mudança

---
+++ Disco.hpp
@@ -0,0 +1,31 @@
+##ifndef DISCO_H
+#define DISCO_H
+
+#include <vector>
+#include <iostream>
+#include <string>
+
+#include "Page.hpp"
+#include "Token.hpp"
+
+using namespace std;
+
+class Disco {
+	private:
+		int id;
+
+		vector<Page> pages;
+	public:
+		Disco();
+		Disco(int id, int page);
+		
+		int getId();
+		void imprimePages();
+		
+		int buscarPage(int page);
+		void retornaParaDisco(int id, int page, int valor);
+		
+		void setPages(Page p);
+		void setId(int id);
+};
+#endif

03e772610b5b106ed246d8cccddcf3e51f03b253
juliamellolopes <juliamellolopesgoncalves@gmail.com>
2021-07-11 11:00:02 -0300

    Nova mudança

--- Trabalho/main.cpp
+++ Trabalho/main.cpp
@@ -37,19 +37,19 @@ int main(){
 
 //	exibe a matriz de processos
 	cout << endl << "MATRIZ SWAP COM PROCESSOS" << endl << endl;
-	for(int i = 0; i < 4; i++) {
+	for(int i = 0; i < 4; i++){
 		cout << "P" << s[i].getId() << ": ";
 		s[i].imprimeProcessos();
 		cout << endl;
 	}
 	
-	for(int i = 0; i < 4; i++) {
+	for(int i = 0; i < 4; i++){
 		carregaMemoria(d, m, &m[i], &s[i], i+1);
 	}
 	
 	//	exibe a matriz disco apoz a manutencao
 	cout << endl << "DISCO APOZ A MANUTENCAO" << endl << endl;
-	for(int i = 0; i < 4; i++) {
+	for(int i = 0; i < 4; i++){
 		cout << "A" << d[i].getId() << ": ";
 		d[i].imprimePages();
 		cout << endl;
@@ -64,22 +64,22 @@ int main(){
 	}
 	
 	cout << "\nPAGE MISS: ";
-	for(int i=0; i < 1; i++ ) {
+	for(int i=0; i < 1; i++ ){
 		cout << "P1: " << m[i].getContP1() << " - P2: " << m[i].getContP2() << " - P3: " << m[i].getContP3() << " - P4: " << m[i].getContP4() << endl;
 	}
 	
 	cout << "FILES REQUEST: ";
-	for(int i=0; i < 1; i++ ) {
+	for(int i=0; i < 1; i++ ){
 		cout << "A1: " << m[i].getContA1() << " - A2: " << m[i].getContA2() << " - A3: " << m[i].getContA3() << " - A4: " << m[i].getContA4() << endl;
 	}
 	
-	for(int i=0; i < 4; i++ ) {
+	for(int i=0; i < 4; i++ ){
 		m[i].liberarMemoria(d);
 	}
 	
 	//	exibe a matriz disco apoz a liberacao da memoria
 	cout << endl << "DISCO APOZ LIBERACAO DA MEMORIA" << endl << endl;
-	for(int i = 0; i < 4; i++) {
+	for(int i = 0; i < 4; i++){
 		cout << "A" << d[i].getId() << ": ";
 		d[i].imprimePages();
 		cout << endl;
@@ -87,25 +87,24 @@ int main(){
 	cout << endl;
 }
 
-void carregaMemoria(Disco *d, Memoria *m, Memoria *m2, Swap *s, int aux) {
+void carregaMemoria(Disco *d, Memoria *m, Memoria *m2, Swap *s, int aux){
 	vector<Processo> proc = s->getProcessos();
 	Processo p;
-	
 	int x, valor;
 
-	for(int i = 0; i < proc.size(); i++) {
-		if(m2->tamProcessos() == 5) {
+	for(int i = 0; i < proc.size(); i++){
+		if(m2->tamProcessos() == 5){
 			m->liberarPrimeiroValor(&p);
 		}
 			
 		x = procurarNaMemoria(m, proc[i].getId(), proc[i].getPage());
 
-		if(x == -1) {
+		if(x == -1){
 			valor = procurarNoDisco(d, proc[i].getId(), proc[i].getPage());
-			if(valor != -1) {
+			if(valor != -1){
 				proc[i].setValor(valor);
 			}
-		} else {
+		} else{
 			proc[i].setValor(x);
 			aux = 0;
 		}
@@ -113,25 +112,23 @@ void carregaMemoria(Disco *d, Memoria *m, Memoria *m2, Swap *s, int aux) {
 	}
 }
 
-int procurarNaMemoria(Memoria *m, int id, int pagina) {
+int procurarNaMemoria(Memoria *m, int id, int pagina){
 	int p;
 	
-	for(int i = 0; i < 4; i++) {
+	for(int i = 0; i < 4; i++){
 		p = m[i].buscarPage(id, pagina);
-		
 		if(p != -1) 
 			return p;
 	}
 	return -1;
 }
 
-int procurarNoDisco(Disco *d, int id, int pagina) {
+int procurarNoDisco(Disco *d, int id, int pagina){
 	int p;
 	
-	for(int i = 0; i < 4; i++) {
-		if(id == d[i].getId()) {
+	for(int i = 0; i < 4; i++){
+		if(id == d[i].getId()){
 			p = d[i].buscarPage(pagina);
-			
 			if(p != -1)
 				return p;
 		}
@@ -139,18 +136,15 @@ int procurarNoDisco(Disco *d, int id, int pagina) {
 	return -1;
 }
 
-void openFile(char *fileNamePages, Disco *d, Swap *s, bool aux) {
+void openFile(char *fileNamePages, Disco *d, Swap *s, bool aux){
 	FILE *pfile = fopen(fileNamePages,"r");
 	char *result;
 	char *tk;
 	char strfile[MAX];
-
 	int tokens[MAX];
 	int cont;
-	
 	Page p;
 	Processo pc;
-	
 	int quantDisco = 0;
 	int quantSwap  = 0;
 	
@@ -164,23 +158,18 @@ void openFile(char *fileNamePages, Disco *d, Swap *s, bool aux) {
 		
 		if(result){
 			cont = tokenizar(result, tokens);
-			
-			if(aux) {
+			if(aux){
 				d[quantDisco].setId(tokens[0]);
-				
 				for(int i=1; i < cont; i++) {
-					p.setPage(tokens[i]);
-					
+					p.setPage(tokens[i]);	
 					d[quantDisco].setPages(p);
 				}
 				quantDisco++;
-			} else {
+			} else{
 				s[quantSwap].setId(tokens[0]);
-				
-				for(int i=1; i < cont - 1; i+= 2) {
+				for(int i=1; i < cont - 1; i+= 2){
 					pc.setId(tokens[i]);
 					pc.setPage(tokens[i + 1]);
-					
 					s[quantSwap].setProcessos(pc);
 				}
 				quantSwap++;
@@ -190,14 +179,13 @@ void openFile(char *fileNamePages, Disco *d, Swap *s, bool aux) {
 	fclose(pfile);
 }
 
-int tokenizar(char *strfile, int *t) {
+int tokenizar(char *strfile, int *t){
   	const char sep[] = "PA<>, ' '";
 	char *tokens;
 	int cont = 0;
-	
 	tokens = strtok(strfile, sep);
 	
-	while(tokens != NULL) {
+	while(tokens != NULL){
 		t[cont++] = atoi(tokens);
 		tokens = strtok(NULL, sep);
 	}

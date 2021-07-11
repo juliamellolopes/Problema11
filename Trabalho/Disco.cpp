fa75e390551dd74142b6687415a4d1bd16341e83
juliamellolopes <juliamellolopesgoncalves@gmail.com>
2021-07-11 11:12:43 -0300

    Nova mudança

--- Trabalho/Disco.cpp
+++ Trabalho/Disco.cpp
@@ -1,35 +1,35 @@
 #include "Disco.hpp"
 
-Disco::Disco() {
+Disco::Disco(){
 	id = 0;
 }
 
-Disco::Disco(int id, int page) {
+Disco::Disco(int id, int page){
 	setId(id);
 }
 
-int Disco::getId() {
+int Disco::getId(){
 	return this->id;	
 }
 
-void Disco::imprimePages() {
-	for(int i=0; i < pages.size(); i++) {
+void Disco::imprimePages(){
+	for(int i=0; i < pages.size(); i++){
 		cout << " " << pages[i].getPage();
 	}
 }
 
-void Disco::setPages(Page p) {
+void Disco::setPages(Page p){
 	this->pages.push_back(p);
 }
 
-void Disco::setId(int id) {
+void Disco::setId(int id){
 	this->id = id;
 }
 
-int Disco::buscarPage(int page) {
+int Disco::buscarPage(int page){
 	int aux;
-	for(int i=0; i < pages.size(); i++) {
-		if(page == i+1) {
+	for(int i=0; i < pages.size(); i++){
+		if(page == i+1){
 			aux = pages[i].getPage();
 			pages[i].setPage(-1);
 			return aux;
@@ -38,11 +38,11 @@ int Disco::buscarPage(int page) {
 	return -1;
 }
 
-void Disco::retornaParaDisco(int id, int page, int valor) {
-	for(int i=0; i < this->pages.size(); i++) {
-		if(getId() == id) {
-			if(i+1 == page) {
-				if(valor != -1) {
+void Disco::retornaParaDisco(int id, int page, int valor){
+	for(int i=0; i < this->pages.size(); i++){
+		if(getId() == id){
+			if(i+1 == page){
+				if(valor != -1){
 					this->pages[i].setPage(valor);
 				}
 			}
